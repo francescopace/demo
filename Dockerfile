f065b99c8f4254dc4671fb8bc8d5352b635b0c8a
francesco.pace <francesco.pace@gmail.com>
2024-09-22 17:44:21 +0200

    update dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:21-alpine
+FROM node:22.9.0-alpine
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const escapeHtml = require("escape-html");
 
 const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
+
 
 app.get("/", (req, res) => {
   return res.status(200).send({
@@ -9,8 +16,17 @@ app.get("/", (req, res) => {
   });
 });
 
+// Cross-Site Scripting (XSS) Vulnerabilities: localhost:5000/?name=<script>alert('XSS')</script>
+app.get("/xss", (req, res) => {
+  //const name = req.query.name;
+  const name = escapeHtml(req.query.name);
+  res.send(`<h1>Hello, ${name}</h1>`);
+});
+
 app.listen(port, () => {
   console.log("Listening on " + port);
 });
+
+
 
 module.exports = app;
